603a95140fbdeea3b2e37cca498fd916ccfb090a
Juan <stockjuan@gmail.com>
2024-05-13 11:14:05 -0300

    Se finalizo punto 1, Par o Impar.

--- assets/parImpar.js
+++ assets/parImpar.js
@@ -1,14 +1,16 @@
+// Hacer un programa que itere por los numeros del 0 al 10 
+// y por cada uno de ellos que imprima y nos diga 
+// si el numero es par o impar
 
 
-let lista = [
-    "Stöck Juan",
-    31,
-    "pichichi",
-    "Juancho",
-    True,
-    False
-]
+for (let i = 0; i < 10; i ++){
+    if (i === 0 ){
+    console.log('El Cero no se puede definir')
+    }
+    else if (i % 2 == 0){
+        console.log(i, 'Es Par')
+    }else{
+        console.log(i , 'Es Impar.')
+    }
 
-for ( let i = 0; i < lista.length ; i++ ){
-    console.log(i)
 }
