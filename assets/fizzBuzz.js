b058217e211146108db9906c16203aa694a4274e
Juan <stockjuan@gmail.com>
2024-05-13 11:47:48 -0300

    Se finalizo punto 3. FizzBuzz

--- assets/fizzBuzz.js
+++ assets/fizzBuzz.js
@@ -4,3 +4,19 @@
 // b) si el numero es múltiplo de 5, mostrar la palabra "Buzz"
 // c) si el numero es multiplo de 3 y 5, mostrar la palabra "FizzBuzz"
 // d) en los demas casos mostrar solo el número
+
+
+
+for (let i= 1 ; i<=15 ; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log(' FizzBuzz.')
+    }else if(i % 5 == 0){
+        console.log(' Buzz.')
+        }
+    else if (i % 3 == 0){
+        console.log(' Fizz.')
+        }
+    else{
+        console.log( i )
+        }
+}
